6f51f4b9cb15adae5662a6b133f02c3b41996e46
Cristian Giagante <cristiangiagante@hotmail.com>
2022-02-13 15:46:03 -0300

    Initial commit

---
+++ Mapanimal.Mobile/Mapanimal.Mobile/SurveyDetailsView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Mapanimal.Mobile.Models;
+using Mapanimal.Mobile.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Mapanimal.Mobile
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SurveyDetailsView : ContentPage
+    {
+        public SurveyDetailsView()
+        {
+            InitializeComponent();
+            MessagingCenter.Subscribe<SurveyDetailsViewModel, Survey>(this, "SaveSurvey", async (a, s) =>
+            {
+                await Navigation.PopModalAsync();
+            });
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            MessagingCenter.Unsubscribe<SurveyDetailsViewModel, Survey>(this, "SaveSurvey");
+        }
+    }
+}
